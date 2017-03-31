776c04a2849b6759751cd3d2f86bf1861ff42619
Wade Wu <wuqingjun@gmail.com>
2017-03-31 00:51:15 -0700

    implemented a simple Producer Consumer class using condition variable and mutex.

--- ProducerConsumer/producerconsumer_wade.h
+++ ProducerConsumer/producerconsumer_wade.h
@@ -1,2 +1,58 @@
 #pragma once
 
+/*
+	a simple implmentation of Producer Consumer using condition variable
+*/
+
+#include <mutex>
+#include <condition_variable>
+#include <vector>
+using namespace std;
+
+template<class T, size_t n = 10>
+class CProducerConsumer
+{
+private:
+	vector<T> mBuffer;
+	int pos;
+	int nSize;
+	mutex mtx;
+	condition_variable cvFull;
+	condition_variable cvEmpty;
+
+public: 
+	CProducerConsumer() : mBuffer(n), nSize(0), pos(0) {}
+
+	void Producer()
+	{
+		while (true)
+		{
+			T t;
+			lock_guard<mutex> lock(mtx);
+			while (nSize == n)
+			{
+				cvFull.wait(lock);
+			}
+			mBuffer[pos] = T;
+			pos = (pos + 1) % n;
+			++nSize;
+			cvEmpty.notify_one();
+		}
+	}
+
+	void Consumer()
+	{
+		while (true)
+		{
+			lock_guard<mutex> lock(mtx);
+			while (nSize == 0)
+			{
+				cvEmpty.wait(lock);
+			}
+			T t = mBuffer[pos];
+			pos = (pos - 1 + n) % n;
+			--nSize;
+			cvFull.notify_one();
+		}
+	}
+};
